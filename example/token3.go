50f55b0c65381fa8f6b44f48e9ec647a4d00cb41
yejiayu <yejiayu.fe@gmail.com>
2018-05-25 17:08:08 +0800

    refactor: Move struct to types

---
+++ example/token3.go
@@ -0,0 +1,418 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package main
+
+import (
+	"math/big"
+	"strings"
+
+	"github.com/cryptape/go-web3/types"
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// TokenABI is the input ABI used to generate the binding from.
+const TokenABI = "[{\"constant\":true,\"inputs\":[{\"name\":\"account\",\"type\":\"address\"}],\"name\":\"getBalance2\",\"outputs\":[{\"name\":\"balance\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"address\"}],\"name\":\"balances\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"name\":\"success\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"account\",\"type\":\"address\"}],\"name\":\"getBalance\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"supplyAmount\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"_from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"_to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"}]"
+
+// TokenBin is the compiled bytecode used for deploying new contracts.
+const TokenBin = `0x608060405234801561001057600080fd5b5060405160208061029d833981016040908152905133600090815260208190529190912055610259806100446000396000f3006080604052600436106100615763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416630a46516b811461006657806327e235e3146100a8578063a9059cbb146100e8578063f8b2cb4f14610119575b600080fd5b34801561007257600080fd5b5061009473ffffffffffffffffffffffffffffffffffffffff60043516610147565b604080519115158252519081900360200190f35b3480156100b457600080fd5b506100d673ffffffffffffffffffffffffffffffffffffffff6004351661014d565b60408051918252519081900360200190f35b3480156100f457600080fd5b5061009473ffffffffffffffffffffffffffffffffffffffff6004351660243561015f565b34801561012557600080fd5b506100d673ffffffffffffffffffffffffffffffffffffffff60043516610205565b50600190565b60006020819052908152604090205481565b33600090815260208190526040812054821180159061017e5750600082115b156101fb57336000818152602081815260408083208054879003905573ffffffffffffffffffffffffffffffffffffffff871680845292819020805487019055805186815290519293927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929181900390910190a35060016101ff565b5060005b92915050565b73ffffffffffffffffffffffffffffffffffffffff16600090815260208190526040902054905600a165627a7a72305820cecbf708828fa24c839101ea6a8f0dcae9a3326563219616d699fb58268610710029`
+
+// DeployToken deploys a new Ethereum contract, binding an instance of Token to it.
+func DeployToken(auth *bind.TransactOpts, backend bind.ContractBackend, supplyAmount *big.Int) (common.Address, *types.Transaction, *Token, error) {
+	parsed, err := abi.JSON(strings.NewReader(TokenABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(TokenBin), backend, supplyAmount)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Token{TokenCaller: TokenCaller{contract: contract}, TokenTransactor: TokenTransactor{contract: contract}, TokenFilterer: TokenFilterer{contract: contract}}, nil
+}
+
+// Token is an auto generated Go binding around an Ethereum contract.
+type Token struct {
+	TokenCaller     // Read-only binding to the contract
+	TokenTransactor // Write-only binding to the contract
+	TokenFilterer   // Log filterer for contract events
+}
+
+// TokenCaller is an auto generated read-only Go binding around an Ethereum contract.
+type TokenCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TokenTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type TokenTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TokenFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type TokenFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TokenSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type TokenSession struct {
+	Contract     *Token            // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// TokenCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type TokenCallerSession struct {
+	Contract *TokenCaller  // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// TokenTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type TokenTransactorSession struct {
+	Contract     *TokenTransactor  // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// TokenRaw is an auto generated low-level Go binding around an Ethereum contract.
+type TokenRaw struct {
+	Contract *Token // Generic contract binding to access the raw methods on
+}
+
+// TokenCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type TokenCallerRaw struct {
+	Contract *TokenCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// TokenTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type TokenTransactorRaw struct {
+	Contract *TokenTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewToken creates a new instance of Token, bound to a specific deployed contract.
+func NewToken(address common.Address, backend bind.ContractBackend) (*Token, error) {
+	contract, err := bindToken(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Token{TokenCaller: TokenCaller{contract: contract}, TokenTransactor: TokenTransactor{contract: contract}, TokenFilterer: TokenFilterer{contract: contract}}, nil
+}
+
+// NewTokenCaller creates a new read-only instance of Token, bound to a specific deployed contract.
+func NewTokenCaller(address common.Address, caller bind.ContractCaller) (*TokenCaller, error) {
+	contract, err := bindToken(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &TokenCaller{contract: contract}, nil
+}
+
+// NewTokenTransactor creates a new write-only instance of Token, bound to a specific deployed contract.
+func NewTokenTransactor(address common.Address, transactor bind.ContractTransactor) (*TokenTransactor, error) {
+	contract, err := bindToken(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &TokenTransactor{contract: contract}, nil
+}
+
+// NewTokenFilterer creates a new log filterer instance of Token, bound to a specific deployed contract.
+func NewTokenFilterer(address common.Address, filterer bind.ContractFilterer) (*TokenFilterer, error) {
+	contract, err := bindToken(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &TokenFilterer{contract: contract}, nil
+}
+
+// bindToken binds a generic wrapper to an already deployed contract.
+func bindToken(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(TokenABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Token *TokenRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Token.Contract.TokenCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Token *TokenRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Token.Contract.TokenTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Token *TokenRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Token.Contract.TokenTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Token *TokenCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Token.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Token *TokenTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Token.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Token *TokenTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Token.Contract.contract.Transact(opts, method, params...)
+}
+
+// Balances is a free data retrieval call binding the contract method 0x27e235e3.
+//
+// Solidity: function balances( address) constant returns(uint256)
+func (_Token *TokenCaller) Balances(opts *bind.CallOpts, arg0 common.Address) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _Token.contract.Call(opts, out, "balances", arg0)
+	return *ret0, err
+}
+
+// Balances is a free data retrieval call binding the contract method 0x27e235e3.
+//
+// Solidity: function balances( address) constant returns(uint256)
+func (_Token *TokenSession) Balances(arg0 common.Address) (*big.Int, error) {
+	return _Token.Contract.Balances(&_Token.CallOpts, arg0)
+}
+
+// Balances is a free data retrieval call binding the contract method 0x27e235e3.
+//
+// Solidity: function balances( address) constant returns(uint256)
+func (_Token *TokenCallerSession) Balances(arg0 common.Address) (*big.Int, error) {
+	return _Token.Contract.Balances(&_Token.CallOpts, arg0)
+}
+
+// GetBalance is a free data retrieval call binding the contract method 0xf8b2cb4f.
+//
+// Solidity: function getBalance(account address) constant returns(balance uint256)
+func (_Token *TokenCaller) GetBalance(opts *bind.CallOpts, account common.Address) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _Token.contract.Call(opts, out, "getBalance", account)
+	return *ret0, err
+}
+
+// GetBalance is a free data retrieval call binding the contract method 0xf8b2cb4f.
+//
+// Solidity: function getBalance(account address) constant returns(balance uint256)
+func (_Token *TokenSession) GetBalance(account common.Address) (*big.Int, error) {
+	return _Token.Contract.GetBalance(&_Token.CallOpts, account)
+}
+
+// GetBalance is a free data retrieval call binding the contract method 0xf8b2cb4f.
+//
+// Solidity: function getBalance(account address) constant returns(balance uint256)
+func (_Token *TokenCallerSession) GetBalance(account common.Address) (*big.Int, error) {
+	return _Token.Contract.GetBalance(&_Token.CallOpts, account)
+}
+
+// GetBalance2 is a free data retrieval call binding the contract method 0x0a46516b.
+//
+// Solidity: function getBalance2(account address) constant returns(balance bool)
+func (_Token *TokenCaller) GetBalance2(opts *bind.CallOpts, account common.Address) (bool, error) {
+	var (
+		ret0 = new(bool)
+	)
+	out := ret0
+	err := _Token.contract.Call(opts, out, "getBalance2", account)
+	return *ret0, err
+}
+
+// GetBalance2 is a free data retrieval call binding the contract method 0x0a46516b.
+//
+// Solidity: function getBalance2(account address) constant returns(balance bool)
+func (_Token *TokenSession) GetBalance2(account common.Address) (bool, error) {
+	return _Token.Contract.GetBalance2(&_Token.CallOpts, account)
+}
+
+// GetBalance2 is a free data retrieval call binding the contract method 0x0a46516b.
+//
+// Solidity: function getBalance2(account address) constant returns(balance bool)
+func (_Token *TokenCallerSession) GetBalance2(account common.Address) (bool, error) {
+	return _Token.Contract.GetBalance2(&_Token.CallOpts, account)
+}
+
+// Transfer is a paid mutator transaction binding the contract method 0xa9059cbb.
+//
+// Solidity: function transfer(_to address, _value uint256) returns(success bool)
+func (_Token *TokenTransactor) Transfer(opts *bind.TransactOpts, _to common.Address, _value *big.Int) (*types.Transaction, error) {
+	return _Token.contract.Transact(opts, "transfer", _to, _value)
+}
+
+// Transfer is a paid mutator transaction binding the contract method 0xa9059cbb.
+//
+// Solidity: function transfer(_to address, _value uint256) returns(success bool)
+func (_Token *TokenSession) Transfer(_to common.Address, _value *big.Int) (*types.Transaction, error) {
+	return _Token.Contract.Transfer(&_Token.TransactOpts, _to, _value)
+}
+
+// Transfer is a paid mutator transaction binding the contract method 0xa9059cbb.
+//
+// Solidity: function transfer(_to address, _value uint256) returns(success bool)
+func (_Token *TokenTransactorSession) Transfer(_to common.Address, _value *big.Int) (*types.Transaction, error) {
+	return _Token.Contract.Transfer(&_Token.TransactOpts, _to, _value)
+}
+
+// TokenTransferIterator is returned from FilterTransfer and is used to iterate over the raw logs and unpacked data for Transfer events raised by the Token contract.
+type TokenTransferIterator struct {
+	Event *TokenTransfer // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *TokenTransferIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(TokenTransfer)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(TokenTransfer)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *TokenTransferIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *TokenTransferIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// TokenTransfer represents a Transfer event raised by the Token contract.
+type TokenTransfer struct {
+	From  common.Address
+	To    common.Address
+	Value *big.Int
+	Raw   types.Log // Blockchain specific contextual infos
+}
+
+// FilterTransfer is a free log retrieval operation binding the contract event 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef.
+//
+// Solidity: e Transfer(_from indexed address, _to indexed address, _value uint256)
+func (_Token *TokenFilterer) FilterTransfer(opts *bind.FilterOpts, _from []common.Address, _to []common.Address) (*TokenTransferIterator, error) {
+
+	var _fromRule []interface{}
+	for _, _fromItem := range _from {
+		_fromRule = append(_fromRule, _fromItem)
+	}
+	var _toRule []interface{}
+	for _, _toItem := range _to {
+		_toRule = append(_toRule, _toItem)
+	}
+
+	logs, sub, err := _Token.contract.FilterLogs(opts, "Transfer", _fromRule, _toRule)
+	if err != nil {
+		return nil, err
+	}
+	return &TokenTransferIterator{contract: _Token.contract, event: "Transfer", logs: logs, sub: sub}, nil
+}
+
+// WatchTransfer is a free log subscription operation binding the contract event 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef.
+//
+// Solidity: e Transfer(_from indexed address, _to indexed address, _value uint256)
+func (_Token *TokenFilterer) WatchTransfer(opts *bind.WatchOpts, sink chan<- *TokenTransfer, _from []common.Address, _to []common.Address) (event.Subscription, error) {
+
+	var _fromRule []interface{}
+	for _, _fromItem := range _from {
+		_fromRule = append(_fromRule, _fromItem)
+	}
+	var _toRule []interface{}
+	for _, _toItem := range _to {
+		_toRule = append(_toRule, _toItem)
+	}
+
+	logs, sub, err := _Token.contract.WatchLogs(opts, "Transfer", _fromRule, _toRule)
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(TokenTransfer)
+				if err := _Token.contract.UnpackLog(event, "Transfer", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
